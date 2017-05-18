43e8fab61f372ce868b7e77239b0377e07ceb83a
Giampaolo Falqui <giampaolofalqui@gmail.com>
2017-05-18 18:28:24 +0200

    Lavora sul supporto di Lumen

--- src/Http/FeedController.php
+++ src/Http/FeedController.php
@@ -2,17 +2,23 @@
 
 namespace Spatie\Feed\Http;
 
+use Illuminate\Http\Request;
 use Spatie\Feed\Feed;
 use Illuminate\Routing\Controller;
 
 class FeedController extends Controller
 {
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
     public function feed()
     {
         $configuration = $this->getFeedConfiguration();
 
         // Overwrite the relative feed url with the request's absolute url
-        $configuration['url'] = request()->url();
+        $configuration['url'] = $this->request->fullUrl();
 
         return (new Feed($configuration))->getFeedResponse();
     }
@@ -21,8 +27,27 @@ class FeedController extends Controller
     {
         $feeds = config('laravel-feed.feeds');
 
-        $feedIndex = (int) str_replace('spatieLaravelFeed', '', app('router')->currentRouteName());
+        $feedIndex = (int) str_replace('spatieLaravelFeed', '', $this->currentRouteName());
 
         return $feeds[$feedIndex] ?? abort(404);
+    }
+
+    private function currentRouteName()
+    {
+        // Laravel 5.x
+        try {
+
+            return app('router')->currentRouteName();
+
+        // Lumen 5.x
+        } catch (\Exception $e) {
+
+            foreach (app()->getRoutes() as $route) {
+                if (strpos($route['uri'], $this->request->path()) !== false) {
+                    return $route['action']['as'];
+                }
+            }
+
+        }
     }
 }
---
+++ src/LumenFeedServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Spatie\Feed;
+
+use Illuminate\View\View;
+use Spatie\Feed\Helpers\Path;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Support\ServiceProvider;
+
+class LumenFeedServiceProvider extends LaravelFeedServiceProvider
+{
+    protected function loadsViews()
+    {
+        $this->publishes([
+            __DIR__.'/../resources/views' => $this->resourcePath('views/vendor/laravel-feed'),
+        ], 'views');
+    }
+
+    protected function registerRouteMacro()
+    {
+        foreach (config('laravel-feed.feeds') as $index => $feedConfiguration) {
+            $this->app->get(
+                Path::merge('', $feedConfiguration['url']),
+                ['as' => "spatieLaravelFeed{$index}", 'uses' => '\Spatie\Feed\Http\FeedController@feed']
+            );
+        }
+    }
+
+    private function resourcePath($path = '')
+    {
+        return $this->app->basePath() . '/resources' . ($path ? '/' . $path : $path);
+    }
+}
